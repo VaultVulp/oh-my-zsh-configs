9c811f017e60418494042763664d3b7c9d1956af
Pavel Alimpiev <VaultVulp@users.noreply.github.com>
2018-05-28 18:05:59 +0300

    Update custom.zsh

--- custom.zsh
+++ custom.zsh
@@ -16,6 +16,12 @@ PURE_GIT_UP_ARROW="↑"
 
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history battery time)
 
+
+
 if hash pipenv 2>/dev/null; then
     source <(pipenv --completion)
+fi
+if hash pyenv 2>/dev/null; then
+    export PATH=$(pyenv root)/shims:$PATH
+    source <($(pyenv root)/completions/pyenv.zsh)
 fi
