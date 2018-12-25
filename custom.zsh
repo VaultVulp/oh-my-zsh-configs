eaae58b7e7f1012d43dc9e9a1da961d707ecb097
Pavel Alimpiev <flammeusvulpes@gmail.com>
2018-12-25 11:34:40 +0300

    Update custom config

--- custom.zsh
+++ custom.zsh
@@ -1,9 +1,6 @@
 ZSH_THEME="powerlevel9k/powerlevel9k"
 plugins=(git docker zsh-completions zsh-autosuggestions zsh-syntax-highlighting)
 
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-
 if hash dircolors 2>/dev/null; then
     eval `dircolors $ZSH_CUSTOM/dircolors.256dark`
 fi
@@ -16,11 +13,4 @@ PURE_GIT_UP_ARROW="↑"
 
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history battery time)
 
-
-
-if hash pipenv 2>/dev/null; then
-    source <(pipenv --completion)
-fi
-if command -v pyenv 1>/dev/null 2>&1; then
-    eval "$(pyenv init -)"
-fi
+source $ZSH_CUSTOM/z.sh
