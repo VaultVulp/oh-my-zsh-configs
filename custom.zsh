29232c58340a33d918994494c001c4a2b5f7480f
Pavel Alimpiev <VaultVulp@users.noreply.github.com>
2019-05-25 16:17:17 +0300

    Update custom.zsh

--- custom.zsh
+++ custom.zsh
@@ -1,11 +1,16 @@
-ZSH_THEME="powerlevel9k/powerlevel9k"
-plugins=(git docker zsh-completions zsh-autosuggestions zsh-syntax-highlighting)
+ZSH_THEME="powerlevel10k/powerlevel10k"
+plugins=(git docker docker-compose zsh-completions zsh-autosuggestions zsh-syntax-highlighting)
 
 if hash dircolors 2>/dev/null; then
     eval `dircolors $ZSH_CUSTOM/dircolors.256dark`
 fi
 
 autoload -U compinit && compinit
+autoload -U +X bashcompinit && bashcompinit
+
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi`
 
 PURE_PROMPT_SYMBOL=">"
 PURE_GIT_DOWN_ARROW="↓"
