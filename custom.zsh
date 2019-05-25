328a1b06fe56732220d107ed9acc345d8296498b
Pavel Alimpiev <flammeusvulpes@gmail.com>
2019-05-25 18:02:39 +0300

    Update configs

--- custom.zsh
+++ custom.zsh
@@ -1,5 +1,5 @@
 ZSH_THEME="powerlevel10k/powerlevel10k"
-plugins=(git docker docker-compose zsh-completions zsh-autosuggestions zsh-syntax-highlighting pipenv pyenv)
+plugins=(git docker docker-compose zsh-completions zsh-autosuggestions zsh-syntax-highlighting pipenv pyenv z)
 
 if hash dircolors 2>/dev/null; then
     eval `dircolors $ZSH_CUSTOM/dircolors.256dark`
@@ -8,11 +8,5 @@ fi
 autoload -U compinit && compinit
 autoload -U +X bashcompinit && bashcompinit
 
-PURE_PROMPT_SYMBOL=">"
-PURE_GIT_DOWN_ARROW="↓"
-PURE_GIT_UP_ARROW="↑"
-
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history battery time)
-
-source $ZSH_CUSTOM/z.sh
+source $ZSH_CUSTOM/purepower.zsh
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
