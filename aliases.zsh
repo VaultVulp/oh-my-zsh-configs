c515ed6aae7fef8edcee3936e800960ac9da675b
Pavel Alimpiev <VaultVulp@users.noreply.github.com>
2019-07-01 15:38:30 +0300

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,6 @@
 alias rm="rm -i"
 alias py="python"
 alias dc="docker-compose"
+alias ld='lazydocker'
 alias budget="code \"$BUDGET_FOLDER\""
 alias blog="docker run --rm -it -p 9999:5000 --volume \"$(pwd)\":/blog vaultvulp/python-2.7-lektor:head"
