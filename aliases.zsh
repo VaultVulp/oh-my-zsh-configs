54661f66d0bcb4a79a539f3432c1feb045abf198
VaultVulp <flammeusvulpes@gmail.com>
2019-12-20 09:22:14 +0300

    Add make-release command

--- aliases.zsh
+++ aliases.zsh
@@ -4,3 +4,5 @@ alias dc="docker-compose"
 alias ld='lazydocker'
 alias budget="code \"${BUDGET_FOLDER}\""
 alias blog='docker run --rm -it -p 5000:5000 --volume `pwd`:/blog vaultvulp/python-2.7-lektor:head'
+
+function make-release { git flow release start -F "$1" && git flow release finish -F -D -p -m "$1" ;}
