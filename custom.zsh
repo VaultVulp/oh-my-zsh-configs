1085682e8ed74f4efb6d89982dd300babd51b173
VaultVulp <me@vaultvulp.dev>
2020-06-04 15:58:02 +0300

    Update custom config

--- custom.zsh
+++ custom.zsh
@@ -1,14 +1,8 @@
 ZSH_THEME="powerlevel10k/powerlevel10k"
-plugins=(git docker docker-compose zsh-completions zsh-autosuggestions zsh-syntax-highlighting pyenv z)
-
-if hash dircolors 2>/dev/null; then
-    eval `dircolors $ZSH_CUSTOM/dircolors.256dark`
-fi
 
 autoload -U compinit && compinit
 autoload -U +X bashcompinit && bashcompinit
 
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
-export PATH="$PATH:/Users/pavelalimpiev/development/flutter/bin"
 source $ZSH_CUSTOM/p10k.zsh
