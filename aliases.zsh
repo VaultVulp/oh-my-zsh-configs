55c14507cd458336cd2887bf53f369d726b52847
VaultVulp <me@vaultvulp.dev>
2023-05-07 18:39:50 +0400

    Bump deps

--- aliases.zsh
+++ aliases.zsh
@@ -4,3 +4,7 @@ alias dc="docker compose"
 alias ld='lazydocker'
 
 function make-release { git flow release start -F "$1" && git flow release finish -F -D -p -m "$1" ;}
+
+alias work="timer 25m && osascript -e 'display notification \"☕\" with title \"Work Timer is up!\" subtitle \"Take a Break 😊\" sound name \"Glass\"'"
+        
+alias rest="timer 5m && osascript -e 'display notification \"☕\" with title \"Break is over!\" subtitle \"Get back to work 😬\" sound name \"Glass\"'"
