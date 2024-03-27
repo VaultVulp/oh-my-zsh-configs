e74cf2378f9645627a239b85665a9d0b339ed631
VaultVulp <me@vaultvulp.dev>
2024-03-27 09:33:04 -0700

    Add custom

--- aliases.zsh
+++ aliases.zsh
@@ -8,3 +8,5 @@ function make-release { git flow release start -F "$1" && git flow release finis
 alias work="timer 25m && osascript -e 'display notification \"☕\" with title \"Work Timer is up!\" subtitle \"Take a Break 😊\" sound name \"Glass\"'"
         
 alias rest="timer 5m && osascript -e 'display notification \"☕\" with title \"Break is over!\" subtitle \"Get back to work 😬\" sound name \"Glass\"'"
+
+alias setup-kernel='poetry run python -m ipykernel install --user --name=`basename $(pwd)`'
